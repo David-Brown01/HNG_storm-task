47530321eeb7cb71752cbea1972513b78748fc3d
David Brown <66221305+David-Brown01@users.noreply.github.com>
2020-06-05 22:02:11 +0100

    Delete Dayved.py
    Delete

--- testScripts/Dayved.py
+++
@@ -1,9 +0,0 @@
-import json
-firstname = "David"
-lastname = "Brown"
-hngid = "HNG -03338"
-language = "Python"
-
-output = "Hello World, this is [" + firstname + "] [" + lastname + "] with HNGi7 ID [" + hngid + " using [" + language + "] for stage 2 task. dayvedbrown257@gmail.com"
-
-print(json.dumps(output))
